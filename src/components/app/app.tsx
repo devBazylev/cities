145998b083d2e0d22006d2fd773df50a6ca5c8c7
jar <dev.bazylev@gmail.com>
2025-05-11 09:10:26 +0300

    feat: add new components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,14 @@
 import Main from '../main/main';
+// import MainEmpty from '../main-empty/main-empty';
 
 type Props = {
   props: number;
 }
 
 function App({props}: Props): JSX.Element {
-  return <Main cardsCount= {props} />;
+  return (
+    <Main cardsCount= {props} />
+  );
 }
 
 export default App;
