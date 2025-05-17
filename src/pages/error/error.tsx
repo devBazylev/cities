c735aa8b9debece53759d1f42b676149557a3d1f
jar <dev.bazylev@gmail.com>
2025-05-17 15:51:54 +0300

    refactor: naming

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -4,20 +4,20 @@ import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
-const DivStyled = styled.div`
+const Div = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
 `;
 
-const H1Styled = styled.h1`
+const H1 = styled.h1`
   margin: 0;
   font-size: 32px;
   color: red;
 `;
 
-const LinkStyled = styled(Link)`
+const Linkk = styled(Link)`
   font-size: 20px;
   color: #3e92ec;
 `;
@@ -31,10 +31,10 @@ function Error(): JSX.Element {
       <Header>
         <Nav />
       </Header>
-      <DivStyled>
-        <H1Styled>404 not found</H1Styled>
-        <LinkStyled to="/">Go to main page</LinkStyled>
-      </DivStyled>
+      <Div>
+        <H1>404 not found</H1>
+        <Linkk to="/">Go to main page</Linkk>
+      </Div>
     </div>
   );
 }
