c4f8fa957a27c4ff437469b7746ddb853ecc2f1c
jar <dev.bazylev@gmail.com>
2025-05-17 12:32:35 +0300

    feat: add forms

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,7 @@
 import Card from '../../components/card/card';
 import Header from '../../components/header/header';
 import Nav from '../../components/nav/nav';
+import FormSorting from '../../components/form-sorting/form-sorting';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { getNextKey } from '../../utils';
@@ -57,21 +58,7 @@ function Main({offersCount}: {offersCount: number}): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offersCount} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <FormSorting />
               <div className="cities__places-list places__list tabs__content">
                 {Array.from({ length: offersCount }, () => (
                   <Card key={getNextKey()} />
