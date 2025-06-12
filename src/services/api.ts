a121a9cf6b326cb51337d337c306d1df079e6cc7
jar <dev.bazylev@gmail.com>
2025-06-12 22:21:32 +0300

    feat: add interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import { Token } from './token';
 
 // const BACKEND_URL = 'https://5c5bc9a6fa9d36c2.mokky.dev';
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
@@ -9,6 +10,18 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = Token.get();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
 
   return api;
 };
