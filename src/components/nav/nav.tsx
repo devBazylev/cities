695c721d870b0317bdbca861fa9d4febd44ce7ee
jar <dev.bazylev@gmail.com>
2025-06-21 13:53:07 +0300

    fix: footer logo && feat: add style to favorites count

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { AuthorizationStatus, AppRoute } from '../../const';
 import { useAppSelector } from '../../hooks';
+import { Span } from './style';
 
 function Nav(): JSX.Element {
   const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
@@ -11,10 +12,10 @@ function Nav(): JSX.Element {
       <ul className="header__nav-list">
         {authorizationStatus === AuthorizationStatus.Auth && (
           <li className="header__nav-item user">
-            <Link to="/login" className="header__nav-link header__nav-link--profile">
+            <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile">
               <div className="header__avatar-wrapper user__avatar-wrapper"></div>
               <span className="header__user-name user__name">{user}</span>
-              <span className="header__favorite-count">3</span>
+              <Span>3</Span>
             </Link>
           </li>
         )}
