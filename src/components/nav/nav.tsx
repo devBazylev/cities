52f8ed2d7705ea7c9945bea5024630876ea68b84
jar <dev.bazylev@gmail.com>
2025-07-29 10:46:22 +0300

    feat: add sign gout

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
 import { AuthorizationStatus, AppRoute } from '../../const';
-import { useAppSelector } from '../../hooks';
+import { useAppSelector, useAppDispatch } from '../../hooks';
 import { Span } from './style';
 import { getAuthorizationStatus, getUser } from '../../store/user-process/selectors';
 import { getFavoriteOffers } from '../../store/site-data/selectors';
+import { logoutUser } from '../../store/api-action';
 
 function Nav(): JSX.Element {
+  const dispatch = useAppDispatch();
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const user = useAppSelector(getUser);
   const favoriteOffers = useAppSelector(getFavoriteOffers);
+
+  const handleSignOutClick = () => {
+    if (authorizationStatus === AuthorizationStatus.Auth) {
+      dispatch(logoutUser());
+    }
+  };
 
   return (
     <nav className="header__nav">
@@ -23,9 +31,15 @@ function Nav(): JSX.Element {
           </li>
         )}
         <li className="header__nav-item">
-          <Link className="header__nav-link" to={AppRoute.Login}>
-            <span className="header__signout">{authorizationStatus === AuthorizationStatus.Auth ? 'Sign out' : 'Sign in'}</span>
-          </Link>
+          {authorizationStatus === AuthorizationStatus.Auth ? (
+            <Link className="header__nav-link" onClick={handleSignOutClick} to={AppRoute.Root}>
+              <span className="header__signout">Sign out</span>
+            </Link>
+          ) : (
+            <Link className="header__nav-link" to={AppRoute.Login}>
+              <span className="header__signout">Sign in</span>
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -3,7 +3,7 @@ import type { CityName, OfferProps, SortName, User, UserAuth, FullOfferProps, Co
 import type { AxiosInstance } from 'axios';
 import { APIRoute, AppRoute } from '../const';
 import { joinPaths } from '../utils';
-import { saveToken } from '../services/token';
+import { saveToken, dropToken } from '../services/token';
 import { History } from 'history';
 
 interface ThunkExtraArg {
@@ -18,6 +18,7 @@ type Extra = {
 
 export const Action = {
   LOGIN_USER: 'user/login',
+  LOGOUT_USER: 'user/logout',
   SET_CITY: 'city/set',
   SET_SORTING: 'sorting/set',
   FETCH_OFFER: 'offers/fetch',
@@ -63,6 +64,23 @@ export const loginUser = createAsyncThunk<UserAuth['email'], UserAuth, { extra: 
     history.push(path);
 
     return email;
+  }
+);
+
+export const logoutUser = createAsyncThunk<void, undefined, { extra: ThunkExtraArg }>(
+  Action.LOGOUT_USER,
+  async (_, { extra }) => {
+    const { api, history } = extra;
+
+    try {
+      await api.delete(APIRoute.Logout);
+    } catch (error) {
+      // Игнорируем ошибки при logout, так как токен все равно нужно удалить
+    }
+
+    dropToken();
+    const path = joinPaths(import.meta.env.BASE_URL || '', AppRoute.Root);
+    history.push(path);
   }
 );
 export const fetchNearbyOffers = createAsyncThunk<FullOfferProps[], FullOfferProps['id'], { extra: ThunkExtraArg }>(
